bfe1b2f686346bc3412cf5e5930f0dab82a44b31
qinning <ning.qin@zentertain.net>
2016-05-09 13:50:16 +0800

    ! fix bug of EXC_BAD_ACCESS Random crashes on Rendered drawBatchedTriangles.

--- cocos/renderer/CCQuadCommand.cpp
+++ cocos/renderer/CCQuadCommand.cpp
@@ -31,6 +31,8 @@
 #include "renderer/CCTechnique.h"
 #include "renderer/CCRenderer.h"
 #include "renderer/CCPass.h"
+#include "base/CCEventDispatcher.h"
+#include "base/CCDirector.h"
 
 #include "xxhash.h"
 
@@ -70,7 +72,32 @@ void QuadCommand::reIndex(int indicesCount)
     if (indicesCount > __indexCapacity)
     {
         CCLOG("cocos2d: QuadCommand: resizing index size from [%d] to [%d]", __indexCapacity, indicesCount);
-        __indices = (GLushort*) realloc(__indices, indicesCount * sizeof(__indices[0]));
+        //__indices = (GLushort*) realloc(__indices, indicesCount * sizeof(__indices[0]));
+        // TODO - Oren Hack fix for this bug - Will cause some leak but won't crash
+        auto tmpindices = __indices;
+        __indices = (GLushort*)malloc(indicesCount * sizeof(__indices[0]));
+        
+        void** listenerHolder = new void*();
+        
+        EventListenerCustom* listener = cocos2d::Director::getInstance()->getEventDispatcher()->addCustomEventListener(cocos2d::Director::EVENT_AFTER_DRAW, [=](cocos2d::EventCustom *event) {
+            
+            if (tmpindices)
+            {
+                free(tmpindices);
+            }
+            
+            // unregister event listener
+            cocos2d::Director::getInstance()->getEventDispatcher()->removeEventListener((EventListener*)*listenerHolder);
+            
+            if (listenerHolder)
+            {
+                delete listenerHolder;
+            }
+            
+        });
+        
+        *listenerHolder = listener;
+        
         __indexCapacity = indicesCount;
     }
 
