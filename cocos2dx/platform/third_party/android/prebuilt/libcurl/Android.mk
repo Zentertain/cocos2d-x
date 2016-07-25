3d30cc91b57c92f0281b1def0a663900b7fba7a8
Pai Deng <pai.deng@zentertain.net>
2016-07-25 18:29:32 +0800

    Update ssl lib to ffulfill the security requirement for GooglePlay.

--- cocos2dx/platform/third_party/android/prebuilt/libcurl/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/libcurl/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_curl_static
 LOCAL_MODULE_FILENAME := curl
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libcurl.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_STATIC_LIBRARIES += cocos_ssl_static
 LOCAL_STATIC_LIBRARIES += cocos_crypto_static
 include $(PREBUILT_STATIC_LIBRARY)
