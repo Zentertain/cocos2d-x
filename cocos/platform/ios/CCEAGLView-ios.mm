cc14c1ea59f655254290bfcce71b56e9bcef7db9
Ricardo Quesada <ricardoquesada@gmail.com>
2016-08-18 18:13:15 -0700

    fix: validates director before layout in iOS (#16394)
    Adds Director::isValid()
    removes DisplayLinkDirector()
    removes virtual functions from Director()
    Director is no longer an abstract class
    
    github issue #14276

--- cocos/platform/ios/CCEAGLView-ios.mm
+++ cocos/platform/ios/CCEAGLView-ios.mm
@@ -253,6 +253,11 @@ Copyright (C) 2008 Apple Inc. All Rights Reserved.
 
 - (void) layoutSubviews
 {
+    if (!cocos2d::Director::getInstance()->isValid())
+    {
+        return;
+    }
+    
     [renderer_ resizeFromLayer:(CAEAGLLayer*)self.layer];
     size_ = [renderer_ backingSize];
 
