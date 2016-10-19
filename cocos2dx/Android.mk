dc9622a155581b66a13b3cd19a55b11ba16a9eb1
马钊 <zhao.ma@zentertain.net>
2016-10-19 12:07:16 +0800

    更新libpng 原因：https://support.google.com/faqs/answer/7011127

--- cocos2dx/Android.mk
+++ cocos2dx/Android.mk
@@ -161,7 +161,8 @@ LOCAL_EXPORT_LDLIBS := -lGLESv2 \
                        -llog \
                        -lz
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos_libpng_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos_libzlib_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_libpng_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_jpeg_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_libxml2_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_libtiff_static
@@ -174,6 +175,7 @@ LOCAL_EXPORT_CFLAGS := -Wno-psabi -DUSE_FILE32API
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,libjpeg)
+$(call import-module,libzlib)
 $(call import-module,libpng)
 $(call import-module,libtiff)
 $(call import-module,libwebp)
